fef9113c21c26b87435f8421ddde0add7b7f20da
Isabel Sánchez Castro <isasancas@alum.us.es>
2026-02-12 11:51:46 +0100

    Inicializacion del repositorio

---
+++ backend/src/test/java/com/compicar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compicar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
