867178fd98ce06ee391215f7bcf36eee8fcd3d3f
davvicval <davvicval@alum.us.es>
2026-03-18 18:15:14 +0100

    feature: Registro realizado

---
+++ backend/src/main/java/com/compicar/autenticacion/registro/Registro.java
@@ -0,0 +1,66 @@
+package com.compicar.autenticacion.registro;
+
+
+public class Registro {
+    
+    String username;
+    
+    String contrasena;
+    
+    String nombre;
+    
+    String apellido;
+    
+    String email;
+    
+    String numTelefono;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumTelefono() {
+        return numTelefono;
+    }
+
+    public void setNumTelefono(String numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+
+}
---
+++ backend/src/main/java/com/compicar/autenticacion/registro/RegistroController.java
@@ -0,0 +1,26 @@
+package com.compicar.autenticacion.registro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registro")
+public class RegistroController {
+
+    private final RegistroService registroService;
+
+    @Autowired
+    public RegistroController(RegistroService registroService) {
+        this.registroService = registroService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> register(@RequestBody Registro request) {
+        registroService.registrarPersona(request);
+        return ResponseEntity.ok("Usuario registrado");
+    }
+}
